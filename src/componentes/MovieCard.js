ef54ab83d45a1009be5b859c73988f6e23272531
Yagho <yaghojp@gmail.com>
2025-07-28 16:00:51 -0300

    end

--- src/componentes/MovieCard.js
+++ src/componentes/MovieCard.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 
 import { FaStar } from "react-icons/fa";
 
+import './MovieCard.css'
+
 const imageUrl = process.env.REACT_APP_IMG_API;
 
 const MovieCard = ({movie, showLink = true}) => {
@@ -10,7 +12,7 @@ const MovieCard = ({movie, showLink = true}) => {
         <img src={imageUrl+movie.poster_path} alt={movie.title}/>
         <h2>{movie.title}</h2>
         <p>
-            <FaStar/> {parseFloat(movie.vote_average.toFixed(2))}
+            <FaStar/> {parseFloat(movie.vote_average.toFixed(1))}
         </p>
         {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,7 +1,72 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from 'react-icons/bs';
+
+import MovieCard from '../componentes/MovieCard';
+
+import './Movie.css';
+
+import tmdb from "../services/tmdb";
 
 const Movie = () => {
+
+  const {id} = useParams();
+  const [movie, setMovie] = useState(null); 
+
+  const getMovie = async(url) =>{
+    const response = await tmdb.get(url)
+    setMovie(response.data);
+    console.log(response.data); 
+  };
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+  }
+
+  useEffect(() => {
+    getMovie(`/movie/${id}`);
+  }, [id]);
+
   return (
-    <div>Movie</div>
+    <div className="movie-page">
+      {movie && <>
+        <MovieCard movie={movie} showLink={false}/>
+        <p className="tagline">{movie.tagline}</p>
+        <div className="info">
+          <h3>
+            <BsWallet2/>Orçamento:
+          </h3>
+          <p>{formatCurrency(movie.budget)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsGraphUp/>Receita:
+          </h3>
+          <p>{formatCurrency(movie.revenue)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsHourglassSplit/>Duração:
+          </h3>
+          <p>{movie.runtime} minutos</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsFillFileEarmarkTextFill/>Descrição:
+          </h3>
+          <p>{movie.overview}</p>
+        </div>
+      </>}
+    </div>
   )
 }
 
